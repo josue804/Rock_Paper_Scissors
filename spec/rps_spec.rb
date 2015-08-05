fd777785c6c15b186accac69f7fa4c5921bcf340
Kyle Mellander & Josue Valverde <student@epicodus.com>
2015-08-05 14:18:24 -0700

    Adds test for Player 2 winning

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,8 +1,14 @@
 require('rspec')
 require('rps')
 
-describe('String#rps') do
-  it("returns a win if player 1 wins") do
-    expect(("rock").rps("scissors")).to(eq("Player 1 Wins"))
-  end
-end
+describe('String#rps') {
+
+  it("returns a win if player 1 wins") {
+    expect("rock".rps("scissors")).to(eq("Player 1 Wins"))
+  }
+
+  it("returns a win for player 2 if player 2 wins") {
+    expect("rock".rps("paper")).to(eq("Player 2 Wins"))
+  }
+  
+}
