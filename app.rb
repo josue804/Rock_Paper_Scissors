c296958baa4486767f80a4d94f03ed2070a23bfe
Kyle Mellander & Josue Valverde <student@epicodus.com>
2015-08-05 15:44:36 -0700

    Add page that displays the winner.

--- app.rb
+++ app.rb
@@ -4,12 +4,16 @@ require('./lib/rps.rb')
 also_reload('lib/**/*.rb')
 
 get('/') {
-  if params.fetch(player1) && !params.fetch(player2)
-    @player1 = params.fetch(player1)
+  # If player 1 has chosen
+  if params.has_key?('player1') && !params.has_key?('player2')
+    @player1 = params.fetch('player1')
     @player = 2
-  elsif params.fetch(player2)
-    @player1 = params.fetch(player1)
-    @player2 = params.fetch(player2)
+  # If Player 2 has chosen, display results
+  elsif params.has_key?('player2')
+    @player1 = params.fetch('player1')
+    @player2 = params.fetch('player2')
+    @winner = @player1.rps(@player2)
+  # When no one has chosen
   else
     @player = 1
   end
