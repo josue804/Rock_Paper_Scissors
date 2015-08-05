2c1d2f492be8a19c37196eaaf3a48c3013d3fba6
Kyle Mellander & Josue Valverde <student@epicodus.com>
2015-08-05 14:19:27 -0700

    Adds test for a tie

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -10,5 +10,8 @@ describe('String#rps') {
   it("returns a win for player 2 if player 2 wins") {
     expect("rock".rps("paper")).to(eq("Player 2 Wins"))
   }
-  
+
+  it("returns a tie if both players choose the same option") {
+    expect("rock".rps("rock")).to(eq("It's a Tie"))
+  }
 }
